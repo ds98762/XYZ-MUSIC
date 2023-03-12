b7035de051febb17081aafe8817a1ab3de67ac6b
Dhiman <103347834+ds98762@users.noreply.github.com>
2023-03-12 15:26:30 +0530

    Update start.py

--- ShizukaXMusic/utils/inline/start.py
+++ ShizukaXMusic/utils/inline/start.py
@@ -67,14 +67,14 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/its-star-boi/ShizukaXMusic"),
+                InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/i_dxlvir"),
             ]
         )
     else:
         if GITHUB_REPO:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/its-star-boi/ShizukaXMusic"),
+                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/i_dxlvir"),
                 ]
             )
         if OWNER:
